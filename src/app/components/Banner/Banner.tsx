1036fa0fb117f74b713ddce011f4a63a112d28f2
AmdjedBouzid <amdjedbouzid9@gmail.com>
2025-07-17 08:25:26 +0200

    some changes

--- src/app/components/Banner/Banner.tsx
+++ src/app/components/Banner/Banner.tsx
@@ -104,7 +104,7 @@ const BannerSlider = () => {
               {/* Main Image */}
               <div className="relative w-full h-[500px] rounded-2xl overflow-hidden shadow-ecommerce animate-float">
                 <img
-                  src={assets.bannerImg.src}
+                  src="/assets/banner/banner2.jpg"
                   alt="Fashion Collection"
                   className="w-full h-full object-cover"
                 />
